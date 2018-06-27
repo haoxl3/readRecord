5052ba95d1152e866dabe36feeffa01ec104424e
haoxl3 <1098262516@qq.com>
2018-06-28 00:09:31 +0800

    me

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
+<template>
+  <button @click="getUserInfo">用户授权信息</button>
+</template>
 <script>
 import {get, showSuccess} from './util'
 import qcloud from 'wafer2-client-sdk'
 import config from './config'
 export default {
-  async created () {
-    let user = wx.getStorageSync('userinfo')
-    if(!user) {
-      qcloud.setLoginUrl(config.loginUrl)
-      qcloud.login({
-        success: function(userinfo) {
-          console.log('登录成功', userinfo)
-          showSuccess('登录成功')
-          wx.setStorageSync('userinfo', userinfo)
-        },
-        fail: function(err) {
-          console.log('登录失败', err)
-        }
-      })
-    }
-    // const res = await get('/weapp/demo')
-    // console.log(123, res)
-  }
+  // async created () {
+  //   let user = wx.getStorageSync('userinfo')
+  //   console.log('user='+user)
+  //   if(!user) {
+  //     qcloud.setLoginUrl(config.loginUrl)
+  //     qcloud.login({
+  //       success: function(userinfo) {
+  //         console.log('登录成功', userinfo)
+  //         showSuccess('登录成功')
+  //         wx.setStorageSync('userinfo', userinfo)
+  //       },
+  //       fail: function(err) {
+  //         console.log('登录失败---', err)
+  //       }
+  //     })
+  //   }
+  //   const res = await get('/weapp/demo')
+  //   console.log(123, res)
+  // }
+  // methods: {
+  //   async getUserInfo() {
+  //     let user = wx.getStorageSync('userinfo')
+  //     console.log('user='+user)
+  //     if(!user) {
+  //       qcloud.setLoginUrl(config.loginUrl)
+  //       qcloud.login({
+  //         success: function(userinfo) {
+  //           console.log('登录成功', userinfo)
+  //           showSuccess('登录成功')
+  //           wx.setStorageSync('userinfo', userinfo)
+  //         },
+  //         fail: function(err) {
+  //           console.log('登录失败---', err)
+  //         }
+  //       })
+  //     }
+  //     const res = await get('/weapp/demo')
+  //     console.log(123, res)
+  //   }
+  // }
 }
 </script>
 
--- src/pages/me/Me.vue
+++ src/pages/me/Me.vue
@@ -5,15 +5,23 @@
             <p>{{userinfo.nickName}}</p>
         </div>
         <YearProgress></YearProgress>
+        <button open-type="getUserInfo" lang="zh_CN" @click="getUserInfo">获取用户信息</button>
         <button class="btn" @click="scanBook">添加图书</button>    
     </div>
 </template>
 <script>
+import {get, showSuccess} from '@/util'
+import qcloud from 'wafer2-client-sdk/index.js'
+import config from '@/config'
+import YearProgress from '@/components/YearProgress'
 export default {
     data() {
         return {
             userinfo: {}
         }
+    },
+    components: {
+        YearProgress
     },
     methods: {
         scanBook() {
@@ -22,6 +30,35 @@ export default {
                     console.log(res)
                 }
             })
+        },
+        async getUserInfo() {
+            let user = wx.getStorageSync('userinfo')
+            console.log('user='+user)
+            qcloud.setLoginUrl(config.loginUrl)
+            console.log(config.loginUrl)
+            if(user) {
+                qcloud.loginWithCode({
+                    success: res => {
+                        console.log(res)
+                        showSuccess('登录成功')
+                    },
+                    fail: err => {
+                        console.error(err)
+                    }
+                })
+            } else {
+                // 首次登录
+                qcloud.login({
+                    success: res => {
+                        showSuccess('登录成功')
+                        console.log(res)
+                        this.userinfo = res 
+                    },
+                    fail: err => {
+                        console.log('登录失败---', err)
+                    }
+                })
+            }
         }
     },
     created() {
@@ -31,9 +68,17 @@ export default {
 </script>
 <style lang="scss">
 .container{
-    padding: 0 30rpx;
-    .userinfo{
-        margin-top: 100rpx;
+    padding:0 30rpx;
+
+}  
+.userinfo{
+    margin-top:100rpx;
+    text-align:center;
+    img{
+        width: 150rpx;
+        height:150rpx;
+        margin: 20rpx;
+        border-radius: 50%;
     }
 }
 </style>
