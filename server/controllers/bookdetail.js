f61fb18a6ab4a980b3cd79f4200a7bacb3973cdd
haoxl3 <1098262516@qq.com>
2018-06-30 20:08:16 +0800

    图书详情

--- server/controllers/bookdetail.js
+++ server/controllers/bookdetail.js
@@ -10,12 +10,13 @@ module.exports = async (ctx) => {
     // user_info为字符串，需要转换
     const info = JSON.parse(detail.user_info)
     ctx.state.data = Object.assign({},detail,{
+        tags: detail.tags.split(','),
+        summary: detail.summary.split('\n'),
         user_info: {
             name: info.nickName,
             image: info.avatarUrl
         }
     })
-    ctx.state.data = detail
     await mysql('books')
         .where('id', id)
         .increment('count', 1)
