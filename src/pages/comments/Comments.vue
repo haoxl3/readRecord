446c8f1f70120a3bcee1b37d1d5b7ab16ab0961c
haoxl3 <1098262516@qq.com>
2018-07-01 16:14:57 +0800

    列表跳到详情

--- src/pages/comments/Comments.vue
+++ src/pages/comments/Comments.vue
@@ -1,10 +1,41 @@
 <template>
-    <div>个人中心2</div>
+    <div>
+        <comment-list type='user' :comments="comments"></comment-list>
+    </div>
 </template>
 <script>
+import {get} from '@/util'
+import CommentList from '@/components/CommentList'
 export default {
-    created() {
-        
+    data() {
+        return {
+            comments: [],
+            userinfo: {}
+        }
+    },
+    components: {
+        CommentList
+    },
+    methods: {
+        init() {
+            wx.showNavigationBarLoading()
+            this.getComments()
+        },
+        async getComments() {
+            const comments = await get('/weapp/commentlist',{
+                openid: this.userinfo.openId
+            })
+            this.comments = comments.list
+        }
+    },
+    onShow() {
+        if(!this.userinfo.openId) {
+            let userinfo = wx.getStorageSync('userinfo')
+            if(userinfo) {
+                this.userinfo = userinfo
+                this.init()
+            }
+        }
     }
 }
 </script>
