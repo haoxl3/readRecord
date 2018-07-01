b640045ce6c37fd204ff28f2b97a9df1477b3267
haoxl3 <1098262516@qq.com>
2018-07-01 17:14:01 +0800

    评论页

--- src/pages/comments/Comments.vue
+++ src/pages/comments/Comments.vue
@@ -1,32 +1,51 @@
 <template>
     <div>
-        <comment-list type='user' :comments="comments"></comment-list>
+        <comment-list v-if="userinfo.openId" type='user' :comments="comments"></comment-list>
+        <div v-if="userinfo.openId">
+            <Card v-for="book in books" :key="book.id" :book="book"></Card>
+            <div v-if="!books.length">暂时还没有添加过书，快去添加吧！</div>
+        </div>
     </div>
 </template>
 <script>
 import {get} from '@/util'
 import CommentList from '@/components/CommentList'
+import Card from '@/components/Card'
 export default {
     data() {
         return {
             comments: [],
-            userinfo: {}
+            userinfo: {},
+            books: []
         }
     },
     components: {
-        CommentList
+        CommentList,
+        Card
     },
     methods: {
         init() {
             wx.showNavigationBarLoading()
             this.getComments()
+            this.getBooks()
+            wx.hideNavigationBarLoading()
         },
         async getComments() {
             const comments = await get('/weapp/commentlist',{
                 openid: this.userinfo.openId
             })
             this.comments = comments.list
+        },
+        async getBooks() {
+            const books = await get('/weapp/booklist', {
+                openid: this.userinfo.openId
+            })
+            this.books = books.list
         }
+    },
+    onPullDownRefresh() {
+        tis.init()
+        wx.stopPullDownRefresh()
     },
     onShow() {
         if(!this.userinfo.openId) {
